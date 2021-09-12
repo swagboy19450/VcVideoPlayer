602a4aafc9e5278cdaaf382524d47b84d00ccc60
丂ﾑﾶﾶƳ <89745675+Sammy-XD@users.noreply.github.com>
2021-09-12 11:11:12 +0530

    Update videoplayer.py

--- player/videoplayer.py
+++ player/videoplayer.py
@@ -22,7 +22,7 @@ call_py = PyTgCalls(app)
 FFMPEG_PROCESSES = {}
 def raw_converter(dl, song, video):
     return subprocess.Popen(
-        ['ffmpeg', '-i', dl, '-f', 's16le', '-ac', '1', '-ar', '48000', song, '-y', '-f', 'rawvideo', '-r', '20', '-pix_fmt', 'yuv420p', '-vf', 'scale=1280:720', video, '-y'],
+        ['ffmpeg', '-i', dl, '-f', 's16le', '-ac', '1', '-ar', '48000', song, '-y', '-f', 'rawvideo', '-r', '20', '-pix_fmt', 'yuv420p', '-vf', 'scale=640:360', video, '-y'],
         stdin=None,
         stdout=None,
         stderr=None,
