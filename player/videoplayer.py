6689711d6bcea157f265dad3ed4cff0281944a4f
丂ﾑﾶﾶƳ <89745675+Sammy-XD@users.noreply.github.com>
2021-09-14 18:26:35 +0530

    Update videoplayer.py

--- player/videoplayer.py
+++ player/videoplayer.py
@@ -5,10 +5,7 @@ import subprocess
 from pytgcalls import idle
 from pytgcalls import PyTgCalls
 from pytgcalls import StreamType
-from pytgcalls.types.input_stream import AudioParameters
-from pytgcalls.types.input_stream import InputAudioStream
-from pytgcalls.types.input_stream import InputVideoStream
-from pytgcalls.types.input_stream import VideoParameters
+from pytgcalls.types.input_stream import AudioVideoPiped
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import API_ID, API_HASH, SESSION_NAME,ADMIN,CHANNEL
@@ -19,14 +16,6 @@ from youtubesearchpython import VideosSearch
 app = Client(SESSION_NAME, API_ID, API_HASH)
 call_py = PyTgCalls(app)
 FFMPEG_PROCESSES = {}
-def raw_converter(dl, song, video):
-    subprocess.Popen(
-        ['ffmpeg', '-i', dl, '-f', 's16le', '-ac', '1', '-ar', '48000', song, '-y', '-f', 'rawvideo', '-r', '20', '-pix_fmt', 'yuv420p', '-vf', 'scale=640:360', video, '-y'],
-        stdin=None,
-        stdout=None,
-        stderr=None,
-        cwd=None,
-    )
 opts = {"format": "best[height=?480]/best", "noplaylist": True}
 ydl = YoutubeDL(opts)
 
@@ -65,30 +54,13 @@ async def stream(client, m: Message):
                 msg = await m.reply_photo(photo=photoid, caption="`Starting Video Stream...`")
                     
             chat_id = m.chat.id
-            process = raw_converter(livelink, f'audio{chat_id}.raw', f'video{chat_id}.raw')
-            FFMPEG_PROCESSES[chat_id] = process
-            await asyncio.sleep(10)
+            await asyncio.sleep(3)
             try:
-                audio_file = f'audio{chat_id}.raw'
-                video_file = f'video{chat_id}.raw'
-                while not os.path.exists(audio_file) or \
-                        not os.path.exists(video_file):
-                    await asyncio.sleep(2)
+                video_file = livelink
                 await call_py.join_group_call(
                     chat_id,
-                    InputAudioStream(
-                        audio_file,
-                        AudioParameters(
-                            bitrate=48000,
-                        ),
-                    ),
-                    InputVideoStream(
-                        video_file,
-                        VideoParameters(
-                            width=640,
-                            height=360,
-                            frame_rate=20,
-                        ),
+                    AudioVideoPiped(
+                        video_file,    
                     ),
                     stream_type=StreamType().local_stream,
                 )
@@ -107,29 +79,13 @@ async def stream(client, m: Message):
         msg = await m.reply_photo(photo=photoid, caption="`Downloading...`")
         video = await client.download_media(m.reply_to_message)
         chat_id = m.chat.id
-        await msg.edit_caption("`Processing...`")
-        os.system(f"ffmpeg -i '{video}' -f s16le -ac 1 -ar 48000 'audio{chat_id}.raw' -y -f rawvideo -r 20 -pix_fmt yuv420p -vf scale=640:360 'video{chat_id}.raw' -y")
         try:
-            audio_file = f'audio{chat_id}.raw'
-            video_file = f'video{chat_id}.raw'
-            while not os.path.exists(audio_file) or \
-                    not os.path.exists(video_file):
-                await asyncio.sleep(2)
+            video_file = video 
+            await asyncio.sleep(2)
             await call_py.join_group_call(
                 chat_id,
-                InputAudioStream(
-                    audio_file,
-                    AudioParameters(
-                        bitrate=48000,
-                    ),
-                ),
-                InputVideoStream(
+                AudioVideoPiped(
                     video_file,
-                    VideoParameters(
-                        width=640,
-                        height=360,
-                        frame_rate=20,
-                    ),
                 ),
                 stream_type=StreamType().local_stream,
             )
@@ -145,13 +101,6 @@ async def stream(client, m: Message):
 async def stopvideo(client, m: Message):
     chat_id = m.chat.id
     try:
-        process = FFMPEG_PROCESSES.get(chat_id)
-        if process:
-            try:
-                process.send_signal(SIGINT)
-                await asyncio.sleep(3)
-            except Exception as e:
-                print(e)
         await call_py.leave_group_call(chat_id)
         await m.reply("**⏹️ Stop Video Stream!**")
     except Exception as e:
@@ -182,33 +131,15 @@ async def chstream(client, m: Message):
                     return
             else:
                 livelink = query
-                msg = await m.reply("`Starting Video Stream...`")
-                    
+                msg = await m.reply("`Starting Video Stream...`")    
             chat_id = CHANNEL
-            process = raw_converter(livelink, f'audio{chat_id}.raw', f'video{chat_id}.raw')
-            FFMPEG_PROCESSES[chat_id] = process
-            await asyncio.sleep(10)
+            await asyncio.sleep(3)
             try:
-                audio_file = f'audio{chat_id}.raw'
-                video_file = f'video{chat_id}.raw'
-                while not os.path.exists(audio_file) or \
-                        not os.path.exists(video_file):
-                    await asyncio.sleep(2)
+                video_file = livelink
                 await call_py.join_group_call(
                     chat_id,
-                    InputAudioStream(
-                        audio_file,
-                        AudioParameters(
-                            bitrate=48000,
-                        ),
-                    ),
-                    InputVideoStream(
-                        video_file,
-                        VideoParameters(
-                            width=640,
-                            height=360,
-                            frame_rate=20,
-                        ),
+                    AudioVideoPiped(
+                        video_file,    
                     ),
                     stream_type=StreamType().local_stream,
                 )
@@ -221,29 +152,13 @@ async def chstream(client, m: Message):
         msg = await m.reply("`Downloading...`")
         video = await client.download_media(m.reply_to_message)
         chat_id = CHANNEL
-        await msg.edit("`Processing...`")
-        os.system(f"ffmpeg -i '{video}' -f s16le -ac 1 -ar 48000 'audio{chat_id}.raw' -y -f rawvideo -r 20 -pix_fmt yuv420p -vf scale=640:360 'video{chat_id}.raw' -y")
         try:
-            audio_file = f'audio{chat_id}.raw'
-            video_file = f'video{chat_id}.raw'
-            while not os.path.exists(audio_file) or \
-                    not os.path.exists(video_file):
-                await asyncio.sleep(2)
+            video_file = video 
+            await asyncio.sleep(2)
             await call_py.join_group_call(
                 chat_id,
-                InputAudioStream(
-                    audio_file,
-                    AudioParameters(
-                        bitrate=48000,
-                    ),
-                ),
-                InputVideoStream(
+                AudioVideoPiped(
                     video_file,
-                    VideoParameters(
-                        width=640,
-                        height=360,
-                        frame_rate=20,
-                    ),
                 ),
                 stream_type=StreamType().local_stream,
             )
@@ -258,13 +173,6 @@ async def chstream(client, m: Message):
 async def chstopvideo(client, m: Message):
     chat_id = CHANNEL
     try:
-        process = FFMPEG_PROCESSES.get(chat_id)
-        if process:
-            try:
-                process.send_signal(SIGINT)
-                await asyncio.sleep(3)
-            except Exception as e:
-                print(e)
         await call_py.leave_group_call(chat_id)
         await m.reply("**⏹️ Stop Channel Stream!**")
     except Exception as e:
